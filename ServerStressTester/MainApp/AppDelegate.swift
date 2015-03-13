5a5ba5e6dc4789ecbf69e99c7c7f7593e1a29947
Vlad-R <Vlad-R@users.noreply.github.com>
2015-03-14 01:35:15 +0200

    Added project base

---
+++ ServerStressTester/MainApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  ServerStressTester
+//
+//  Created by Vlad-R on 14/03/15.
+//  Copyright (c) 2015 Vlad-R. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+}
---
+++ ServerStressTester/MainApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ServerStressTester
+//
+//  Created by Vlad-R on 14/03/15.
+//  Copyright (c) 2015 Vlad-R. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
